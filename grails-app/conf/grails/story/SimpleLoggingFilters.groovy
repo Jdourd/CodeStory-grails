70e30706b2f5f3c3d92939ecbf15a30e09f9da07
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-09 22:13:02 +0100

    init

---
+++ grails-app/conf/grails/story/SimpleLoggingFilters.groovy
@@ -0,0 +1,42 @@
+package grails.story
+
+import java.util.logging.Logger
+
+class SimpleLoggingFilters {
+
+	private static final loggingFilter = Logger.getLogger('ut')
+
+	def filters = {
+		all(uri:'/**') {
+			before = {
+			  
+			  	def logString = request.getMethod() + " " + request.getRequestURI() + " "
+				
+				def params = request.getParameterMap()
+				if(!params.isEmpty()) {
+					logString += params
+				}
+			  	
+			  	def contentLength = request.getContentLength()
+				if(contentLength > 0) {
+					def data = retrieveBody(request)
+					logString += contentLength + " " + data
+				}
+			    
+				loggingFilter.info(logString)
+			}
+		}
+	}
+	
+	String retrieveBody(request) {
+		BufferedReader reader = request.getReader()
+		StringBuilder sb = new StringBuilder()
+	    String line = reader.readLine()
+	    while (line != null) {
+	        sb.append(line + "\n")
+	        line = reader.readLine()
+	    }
+	    reader.close()
+	    return sb.toString()
+	}
+}
---
+++ test/unit/grails/story/SimpleLoggingFiltersTests.groovy
@@ -0,0 +1,13 @@
+package grails.story
+
+
+
+import grails.test.mixin.*
+
+//@Mock(SimpleLoggingFilters)
+class SimpleLoggingFiltersTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
