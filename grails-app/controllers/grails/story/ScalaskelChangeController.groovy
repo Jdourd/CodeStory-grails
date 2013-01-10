a5d88eb296cd11ae0fa154cafdc0faeedae02ca4
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-11 00:23:36 +0100

    First draft

--- grails-app/controllers/grails/story/ScalaskelChangeController.groovy
+++ grails-app/controllers/grails/story/ScalaskelChangeController.groovy
@@ -4,9 +4,126 @@ import grails.converters.JSON
 
 class ScalaskelChangeController {
 	
+	def bazValueInCent = 21
+	def qixValueInCent = 11
+	def barValueInCent = 7
+	
 	def json = {
-		def map= [[foo:params.value]]
-		
+		def centToChange = params.value as int
+		def simpleCombinaisons = getSimpleCombinaisons(centToChange)
+		def map = getOtherCombination(simpleCombinaisons)
 		render(map as JSON)
+	}
+	
+	def getSimpleCombinaisons(centToChange) {
+		getSimpleBazCombinaison(centToChange)
+		getSimpleQixCombinaison(centToChange)
+		getSimpleBarCombinaison(centToChange)
+	}
+
+	def getSimpleBazCombinaison(int centToChange) {
+		def baz = (centToChange / bazValueInCent) as int
+		def restInCent = centToChange - baz * bazValueInCent
+		def bazAndQix = getSimpleQixCombinaison(restInCent)
+		bazAndQix.baz = baz
+		def bazAndBar = getSimpleBarCombinaison(restInCent)
+		bazAndBar.baz = baz
+		return [bazAndQix, bazAndBar]
+	}
+
+	def getSimpleQixCombinaison(int centToChange) {
+		def qix = (centToChange / qixValueInCent) as int
+		def restInCent = centToChange - qix * qixValueInCent
+		def qixAndBar = getSimpleBarCombinaison(restInCent)
+		qixAndBar.qix = qix
+		return qixAndBar
+	}
+
+	def getSimpleBarCombinaison(int centToChange) {
+		def bar = (centToChange / barValueInCent) as int
+		def foo = centToChange - bar * barValueInCent
+		return ["bar":bar, "foo":foo]
+	}
+	
+	def getOtherCombination(existingCombinaisons) {
+		def combinaisons = new HashSet()
+		existingCombinaisons.each {
+			addCombinaison(combinaisons, it.baz, it.qix, it.bar, it.foo)
+			
+		}
+		return combinaisons
+	}
+	
+	def addCombinaison(set, baz, qix, bar, foo) {
+		def change = new HashMap()
+		if(baz > 0){
+			change.baz=baz
+		}
+		if(qix > 0){
+			change.qix=qix
+		}
+		if(bar > 0){
+			change.bar=bar
+		}
+		if(foo > 0){
+			change.foo=foo
+		}
+		println "Map : $change"
+		set.add(change)
+	}
+	
+	def combineBaz(set, baz, qix, bar, foo) {
+		def reduceBazIntoQix = {
+			baz -- // -21
+			if(bar > 1) {
+				qix += 2 // +22
+				bar -- // -7
+				foo += 5 // +5
+			} else if(foo >= 1) {
+				qix += // +22
+				foo -- // -1
+			} else if(foo >= 4) {
+				qix ++ // +11
+				bar +=2 // +14
+				foo -= 4 // -4
+			} else {
+				qix ++ // +11
+				bar ++ // +7
+				foo += 3 // +3
+			}
+			println "Reduce baz into qiz : $baz, $qix, $bar, $foo"
+		}
+		for (; baz >= 0; reduceBazIntoQix()) {
+			println "Baz : $baz, $qix, $bar, $foo"
+			addCombinaison(set, baz, qix, bar, foo)
+			combineQix(set, baz, qix, bar, foo)
+			combineBar(set, baz, qix, bar, foo)
+		}
+	}
+	
+	def combineQix(set, baz, qix, bar, foo) {
+		def reduceQix = {
+			qix -- // -11
+			bar ++ // +7
+			foo += 4 // +4
+			println "Reduce qix : $baz, $qix, $bar, $foo"
+		}
+		for (; qix >= 0; reduceQix()) {
+			println "Qix : $baz, $qix, $bar, $foo"
+			addCombinaison(set, baz, qix, bar, foo)
+			combineBar(set, baz, qix, bar, foo)
+		}
+	}
+	
+	def combineBar(set, baz, qix, bar, foo) {
+		def reduceBar = {
+			bar -- // -7
+			foo += 7 // +7
+			println "Reduce bar : $baz, $qix, $bar, $foo"
+		}
+		for (; bar >= 0; reduceBar()) {
+			println "Bar : $baz, $qix, $bar, $foo"
+			addCombinaison(set, baz, qix, bar, foo)
+		}
 	}
 }
--- test/unit/grails/story/ScalaskelChangeControllerTests.groovy
+++ test/unit/grails/story/ScalaskelChangeControllerTests.groovy
@@ -1,7 +1,7 @@
 package grails.story
 
-
-
+import static org.fest.assertions.Assertions.assertThat
+import grails.converters.JSON
 import grails.test.mixin.*
 import org.junit.*
 
@@ -12,16 +12,50 @@ import org.junit.*
 class ScalaskelChangeControllerTests {
 
     void test_should_change_1_cent(){
-		askAndCheckResponse(1, '[{"foo":1}]')
+		askAndCheckResponse(1, [["foo":1]])
 	}
 
     void test_should_change_2_cent(){
-		askAndCheckResponse(2, '[{"foo":2}]')
+		askAndCheckResponse(2, [["foo":2]])
 	}
+
+    void test_should_change_7_cent(){
+		askAndCheckResponse(7, [["bar":1],["foo":7]])
+	}
+
+    void test_should_change_8_cent(){
+		askAndCheckResponse(8, [["bar":1,"foo":1],["foo":8]])
+	}
+
+    void test_should_change_9_cent(){
+		askAndCheckResponse(9, [["bar":1,"foo":2],["foo":9]])
+	}
+
+    void test_should_change_11_cent(){
+		askAndCheckResponse(11, [["qix":1],["foo":11],["bar":1,"foo":4]])
+	}
+
+    void test_should_change_21_cent(){
+		askAndCheckResponse(21, [
+			["foo":14, "bar":1], ["baz":1], ["qix":1, "foo":3, "bar":1], ["qix":1, "foo":10], ["foo":7, "bar":2], ["foo":21]
+			])
+    }
+
+    void test_should_change_25_cent(){
+		askAndCheckResponse(25, [["baz":1,"foo":4], ["qix":2,"foo":3], ["qix":1,"bar":2], ["qix":1,"bar":1,"foo":7], ["qix":1,"foo":14], ["bar":3,"foo":4], ["bar":2,"foo":11], ["bar":1,"foo":18], ["foo":25]])
+    }
+
+    void test_should_change_55_cent(){
+		askAndCheckResponse(55, [["baz":2,"qix":1,"foo":2], ["baz":1,"qix":3,"foo":1], ["baz":1,"qix":2,"bar":1,"foo":5], ["baz":1,"qix":2,"foo":12], ["baz":1,"qix":1,"bar":3,"foo":2], ["baz":1,"qix":1,"bar":2,"foo":9], ["baz":1,"qix":1,"bar":1,"foo":16], ["baz":1,"qix":1,"foo":23], ["baz":1,"bar":4,"foo":6], ["baz":1,"bar":3,"foo":13], ["baz":1,"bar":2,"foo":20], ["baz":1,"bar":1,"foo":27], ["baz":1,"foo":34], ["qix":5], ["qix":4,"bar":1,"foo":4], ["qix":4,"foo":11], ["qix":3,"bar":3,"foo":1], ["qix":3,"bar":2,"foo":8], ["qix":3,"bar":1,"foo":15], ["qix":3,"foo":22], ["qix":2,"bar":4,"foo":5], ["qix":2,"bar":3,"foo":12], ["qix":2,"bar":2,"foo":19], ["qix":2,"bar":1,"foo":26], ["qix":2,"foo":33], ["qix":1,"bar":6,"foo":2], ["qix":1,"bar":5,"foo":9], ["qix":1,"bar":4,"foo":16], ["qix":1,"bar":3,"foo":23], ["qix":1,"bar":2,"foo":30], ["qix":1,"bar":1,"foo":37], ["qix":1,"foo":44], ["bar":7,"foo":6], ["bar":6,"foo":13], ["bar":5,"foo":20], ["bar":4,"foo":27], ["bar":3,"foo":34], ["bar":2,"foo":41], ["bar":1,"foo":48], ["foo":55]])
+    }
 	
 	def askAndCheckResponse(change, expectedResponse){
 		controller.params.value = change
 		controller.json()
-		assertEquals(expectedResponse, controller.response.contentAsString)
+		def response = JSON.parse(controller.response.contentAsString)
+		expectedResponse.each({
+			assertThat(response).contains(it)
+		})
+		assertThat(response).hasSize(expectedResponse.size())
 	}
 }
