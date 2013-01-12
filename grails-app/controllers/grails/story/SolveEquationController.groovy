8c4ea1c3a64bb04967250a95839a204d9acf2f61
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-12 17:30:46 +0100

    Wrong test leads to wrong behaviour...

--- grails-app/controllers/grails/story/SolveEquationController.groovy
+++ grails-app/controllers/grails/story/SolveEquationController.groovy
@@ -1,22 +1,18 @@
 package grails.story
 
 import org.apache.commons.logging.LogFactory
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
 
 class SolveEquationController {
 
 	private static final logger = LogFactory.getLog(this)
 
     def solve() {
-		ScriptEngineManager mgr = new ScriptEngineManager()
-		ScriptEngine engine = mgr.getEngineByName("JavaScript")
 		def cleanedEquation = params.q.replaceAll(/ +/, '+').replaceAll(',', '.')
 		logger.debug "equation=$cleanedEquation"
-		def BigDecimal result = engine.eval(cleanedEquation)
+		def BigDecimal result = Eval.me(cleanedEquation)
 		logger.debug "result=$result"
 		
-		def resultAsInt = result as int
+		def resultAsInt = result as BigInteger
 		if(result == resultAsInt) {
 			render(text:resultAsInt, contentType:'text/plain')
 		} else {
--- test/unit/grails/story/SolveEquationControllerTests.groovy
+++ test/unit/grails/story/SolveEquationControllerTests.groovy
@@ -23,7 +23,7 @@ class SolveEquationControllerTests {
     }
 	
 	void test_should_return_results_with_big_decimal() {
-       askAndCheckResponse('((1.1+2)+3.14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000','31878018903828901761984975061078744643351263313920')
+       askAndCheckResponse('((1.1+2)+3.14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000','31878018903828899277492024491376690701584023926880')
     }
 	
 	def askAndCheckResponse(equation, expectedResponse){
