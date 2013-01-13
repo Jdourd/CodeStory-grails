a5097932ef25b987eea86a792df2a0794db3f5bc
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-14 00:55:09 +0100

    Move logic test into TripSolverServiceTests. Now test json controller parsing and response unmarshalling in JajascriptControllerTests.

--- test/unit/grails/story/JajascriptControllerTests.groovy
+++ test/unit/grails/story/JajascriptControllerTests.groovy
@@ -11,51 +11,18 @@ import org.junit.*
 class JajascriptControllerTests {
 	
 	void test_optimize_simple_combinaison() {
-		def propositions = [
-          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ]
-		]
+		def propositions = '[{ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 }]'
 		def expectedOptimisation = ["gain" : 10, "path" : ["MONAD42"]]
-		askAndCheckResponse(propositions, expectedOptimisation)
-    }
-	
-	void test_optimize_two_simple_combinaisons() {
-		def propositions = [
-          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
-          [ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 ]
-		]
-		def expectedOptimisation = ["gain" : 18, "path" : ["MONAD42","LEGACY01"]]
-		askAndCheckResponse(propositions, expectedOptimisation)
-    }
-	
-	void test_optimize_two_conflict_combinaisons() {
-		def propositions = [
-          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
-          [ "VOL": "LEGACY01", "DEPART": 0, "DUREE": 9, "PRIX": 18 ]
-		]
-		def expectedOptimisation = ["gain" : 18, "path" : ["LEGACY01"]]
-		askAndCheckResponse(propositions, expectedOptimisation)
-    }
-
-    void test_example_combinaison() {
-		def propositions = [
-          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
-          [ "VOL": "META18", "DEPART": 3, "DUREE": 7, "PRIX": 14 ],
-          [ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 ],
-          [ "VOL": "YAGNI17", "DEPART": 5, "DUREE": 9, "PRIX": 7 ]
-		]
-		def expectedOptimisation = ["gain" : 18, "path" : ["MONAD42","LEGACY01"]]
 		askAndCheckResponse(propositions, expectedOptimisation)
     }
 
     void test_expanded_example_combinaison() {
-		def propositions = [
-          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
-          [ "VOL": "META18", "DEPART": 3, "DUREE": 7, "PRIX": 14 ],
-          [ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 ],
-          [ "VOL": "YAGNI17", "DEPART": 5, "DUREE": 9, "PRIX": 7 ],
-          [ "VOL": "TABOUL1", "DEPART": 0, "DUREE": 20, "PRIX": 20 ],
-          [ "VOL": "PISTAC03", "DEPART": 15, "DUREE": 3, "PRIX": 5 ]
-		]
+		def propositions = '[{ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 },' + 
+							'{ "VOL": "META18", "DEPART": 3, "DUREE": 7, "PRIX": 14 },' + 
+							'{ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 },' + 
+							'{ "VOL": "YAGNI17", "DEPART": 5, "DUREE": 9, "PRIX": 7 },' + 
+							'{ "VOL": "TABOUL1", "DEPART": 0, "DUREE": 20, "PRIX": 20 },' + 
+							'{ "VOL": "PISTAC03", "DEPART": 15, "DUREE": 3, "PRIX": 5 }]'
 		def expectedOptimisation = ["gain" : 23, "path" : ["MONAD42","LEGACY01","PISTAC03"]]
 		askAndCheckResponse(propositions, expectedOptimisation)
     }
@@ -64,7 +31,7 @@ class JajascriptControllerTests {
 		defineBeans {
 			tripSolverService(TripSolverService)
 		}
-		request.json = propositions
+		request.content = propositions
 		controller.optimize()
 		def responseObject = JSON.parse(response.contentAsString)
 		assert responseObject == expectedResponse
--- test/unit/grails/story/TripSolverServiceTests.groovy
+++ test/unit/grails/story/TripSolverServiceTests.groovy
@@ -10,8 +10,54 @@ import org.junit.*
  */
 @TestFor(TripSolverService)
 class TripSolverServiceTests {
+	
+	void test_optimize_simple_combinaison() {
+		def propositions = [
+          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ]
+		]
+		def expectedOptimisation = ["gain" : 10, "path" : ["MONAD42"]]
+		assert service.solveTrips(propositions) == expectedOptimisation
+    }
+	
+	void test_optimize_two_simple_combinaisons() {
+		def propositions = [
+          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
+          [ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 ]
+		]
+		def expectedOptimisation = ["gain" : 18, "path" : ["MONAD42","LEGACY01"]]
+		assert service.solveTrips(propositions) == expectedOptimisation
+    }
+	
+	void test_optimize_two_conflict_combinaisons() {
+		def propositions = [
+          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
+          [ "VOL": "LEGACY01", "DEPART": 0, "DUREE": 9, "PRIX": 18 ]
+		]
+		def expectedOptimisation = ["gain" : 18, "path" : ["LEGACY01"]]
+		assert service.solveTrips(propositions) == expectedOptimisation
+    }
 
-    void testSomething() {
-        fail "Implement me"
+    void test_example_combinaison() {
+		def propositions = [
+          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
+          [ "VOL": "META18", "DEPART": 3, "DUREE": 7, "PRIX": 14 ],
+          [ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 ],
+          [ "VOL": "YAGNI17", "DEPART": 5, "DUREE": 9, "PRIX": 7 ]
+		]
+		def expectedOptimisation = ["gain" : 18, "path" : ["MONAD42","LEGACY01"]]
+		assert service.solveTrips(propositions) == expectedOptimisation
+    }
+
+    void test_expanded_example_combinaison() {
+		def propositions = [
+          [ "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 ],
+          [ "VOL": "META18", "DEPART": 3, "DUREE": 7, "PRIX": 14 ],
+          [ "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 ],
+          [ "VOL": "YAGNI17", "DEPART": 5, "DUREE": 9, "PRIX": 7 ],
+          [ "VOL": "TABOUL1", "DEPART": 0, "DUREE": 20, "PRIX": 20 ],
+          [ "VOL": "PISTAC03", "DEPART": 15, "DUREE": 3, "PRIX": 5 ]
+		]
+		def expectedOptimisation = ["gain" : 23, "path" : ["MONAD42","LEGACY01","PISTAC03"]]
+		assert service.solveTrips(propositions) == expectedOptimisation
     }
 }
