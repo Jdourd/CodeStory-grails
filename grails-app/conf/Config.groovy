80ce3ee350e1bce5f5979f76803e4fadb7fa7dea
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-14 00:11:22 +0100

    Oups, debug mode set...

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -89,5 +89,5 @@ log4j = {
 	info stdout: 'grails.story.ScalaskelChangeController'
 	info stdout: 'grails.story.SolveEquationController'
 	info stdout: 'grails.story.JajascriptController'
-	debug stdout: 'grails.story.TripSolverService'
+	info stdout: 'grails.story.TripSolverService'
 }
