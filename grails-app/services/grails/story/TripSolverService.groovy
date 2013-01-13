2e83867e7ec4d6fa79823f2be6c2589074fa1ef0
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-14 00:39:56 +0100

    Refactoring : separating logic into smaller parts

--- grails-app/services/grails/story/TripSolverService.groovy
+++ grails-app/services/grails/story/TripSolverService.groovy
@@ -14,21 +14,33 @@ class TripSolverService {
 	static transactional = false
 	// A new service will be created per request.
 	static scope = "request"
+	
+	def store = new Store()
+	def vars = []
+	def profit
+	def trips = []
+	def tripWeights = []
+	def optimisation = [gain: 0, path: []]
 
     def solveTrips(jsonTrips) {
 		logger.debug "jsonTrips=$jsonTrips"
-		def store = new Store()
-		def trips = []
-		def tripWeights = []
+		createTripConstraints(jsonTrips)
+		createProfitConstraints()
+		grabAllVars()
+		if(resolveConstraints()) {
+			formatOptimisation(jsonTrips)
+		}
+		logger.debug "optimisation=$optimisation"
+		return optimisation
+    }
+	
+	private createTripConstraints(jsonTrips) {
 		def tripCollisions = []
-		def uniqueTripCollisions = new HashSet()
-		def optimisation = [gain: 0, path: []]
-		
 		jsonTrips.eachWithIndex { object, index ->
-			def trip = new BooleanVar(store, "t$index");
+			def trip = new BooleanVar(store, "t$index")
 			trips.add(trip)
 			tripWeights.add(object.PRIX)
-			
+
 			for(int i = 0; i < object.DUREE; i++){
 				def hour = object.DEPART + i
 				if(tripCollisions[hour] == null) {
@@ -41,55 +53,61 @@ class TripSolverService {
 		logger.debug "tripWeights=$tripWeights"
 		logger.debug "tripCollisions=$tripCollisions"
 		
+		def uniqueTripCollisions = new HashSet()
 		tripCollisions.each {
 			if(it.size >= 2) {
 				uniqueTripCollisions.add(it)
 			}
 		}
 		logger.debug "uniqueTripCollisions=$uniqueTripCollisions"
-		
+
 		uniqueTripCollisions.each {
 			def combinaisonSize = it.size
 			def allZeroCombinaison = [0] * combinaisonSize
 			def possibleCombinaisons = [allZeroCombinaison]
-			
+
 			for(int i = 0; i < combinaisonSize; i++) {
 				def anotherCombinaison = [0] * combinaisonSize
 				anotherCombinaison[i] = 1
 				possibleCombinaisons.add(anotherCombinaison)
 			}
-			
+
 			def varsInCombinaison = []
 			for(int i = 0; i < combinaisonSize; i++) {
 				varsInCombinaison.add(trips[it[i]])
 			}
-			
+
 			store.impose(new ExtensionalSupportVA(varsInCombinaison, possibleCombinaisons as int[][]));
 			logger.debug "varsInCombinaison=$varsInCombinaison"
 			logger.debug "possibleCombinaisons=$possibleCombinaisons"
 		}
-		
-		IntVar profit = new IntVar(store, "profit", 0, IntDomain.MaxInt);
-		store.impose(new SumWeight(trips, tripWeights, profit));
-		
-		def vars = [profit]
+	}
+
+	private createProfitConstraints() {
+		profit = new IntVar(store, "profit", 0, IntDomain.MaxInt);
+		store.impose(new SumWeight(trips, tripWeights, profit))
+	}
+
+	private grabAllVars() {
+		vars.add(profit)
 		vars.addAll(trips)
 		logger.debug "vars=$vars"
-		
+	}
+
+	private boolean resolveConstraints() {
 		Search<IntVar> label = new DepthFirstSearch<IntVar>();
 		label.setPrintInfo(logger.isDebugEnabled());
 		SelectChoicePoint<IntVar> select = new InputOrderSelect<IntVar>(store, vars as IntVar[], new IndomainMax<IntVar>());
-		boolean result = label.labeling(store, select);
-		if ( result ) {
-			def selectedTrips = []
-			trips.eachWithIndex { object, index ->
-				if(object.value() == 1) {
-					selectedTrips.add(jsonTrips[index].VOL)
-				}
+		return label.labeling(store, select);
+	}
+	
+	private formatOptimisation(jsonTrips) {
+		def selectedTrips = []
+		trips.eachWithIndex { object, index ->
+			if(object.value() == 1) {
+				selectedTrips.add(jsonTrips[index].VOL)
 			}
-			optimisation = [gain: profit.value(), path: selectedTrips]
 		}
-		logger.debug "optimisation=$optimisation"
-		return optimisation
-    }
+		optimisation = [gain: profit.value(), path: selectedTrips]
+	}
 }
