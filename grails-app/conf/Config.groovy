e37bdc5e996528f206aa1c5384051c0170caac98
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-14 21:49:00 +0100

    Removing JajascriptController debug mode

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -88,6 +88,6 @@ log4j = {
 	info stdout: 'ut'
 	info stdout: 'grails.story.ScalaskelChangeController'
 	info stdout: 'grails.story.SolveEquationController'
-	debug stdout: 'grails.story.JajascriptController'
+	info stdout: 'grails.story.JajascriptController'
 	info stdout: 'grails.story.TripSolverService'
 }
