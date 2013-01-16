f5d0beec27c39a73aa1a896644e935f4da12c335
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-16 21:35:33 +0100

    Passage du service en singleton + suppression des logs

--- grails-app/services/grails/story/TripSolverService.groovy
+++ grails-app/services/grails/story/TripSolverService.groovy
@@ -13,28 +13,17 @@ class TripSolverService {
 	// Services are transactional by default in Grails. It is not needed here.
 	static transactional = false
 	// A new service will be created per request.
-	static scope = "request"
-	
-	def store = new Store()
-	def vars = []
-	def profit
-	def trips = []
-	def tripWeights = []
-	def optimisation = [gain: 0, path: []]
+	static scope = "singleton"
 
     def solveTrips(jsonTrips) {
-		logger.debug "jsonTrips=$jsonTrips"
-		createTripConstraints(jsonTrips)
-		createProfitConstraints()
-		grabAllVars()
-		if(resolveConstraints()) {
-			formatOptimisation(jsonTrips)
-		}
-		logger.debug "optimisation=$optimisation"
-		return optimisation
-    }
+		def store = new Store()
+		def vars = []
+		def profit
+		def trips = []
+		def tripWeights = []
+		def optimisation = [gain: 0, path: []]
 	
-	private createTripConstraints(jsonTrips) {
+//		logger.debug "jsonTrips=$jsonTrips"
 		def tripCollisions = []
 		jsonTrips.sort{ it.DEPART }
 				 .eachWithIndex { object, index ->
@@ -50,9 +39,9 @@ class TripSolverService {
 				tripCollisions[hour].add(index)
 			}
 		}
-		logger.debug "trips=$trips"
-		logger.debug "tripWeights=$tripWeights"
-		logger.debug "tripCollisions=$tripCollisions"
+//		logger.debug "trips=$trips"
+//		logger.debug "tripWeights=$tripWeights"
+//		logger.debug "tripCollisions=$tripCollisions"
 		
 		def uniqueTripCollisions = new HashSet()
 		tripCollisions.each {
@@ -60,7 +49,7 @@ class TripSolverService {
 				uniqueTripCollisions.add(it)
 			}
 		}
-		logger.debug "uniqueTripCollisions=$uniqueTripCollisions"
+//		logger.debug "uniqueTripCollisions=$uniqueTripCollisions"
 
 		uniqueTripCollisions.each {
 			def combinaisonSize = it.size
@@ -79,36 +68,27 @@ class TripSolverService {
 			}
 
 			store.impose(new ExtensionalSupportVA(varsInCombinaison, possibleCombinaisons as int[][]));
-			logger.debug "varsInCombinaison=$varsInCombinaison"
-			logger.debug "possibleCombinaisons=$possibleCombinaisons"
+//			logger.debug "varsInCombinaison=$varsInCombinaison"
+//			logger.debug "possibleCombinaisons=$possibleCombinaisons"
 		}
-	}
-
-	private createProfitConstraints() {
 		profit = new IntVar(store, "profit", 0, IntDomain.MaxInt);
 		store.impose(new SumWeight(trips, tripWeights, profit))
-	}
-
-	private grabAllVars() {
 		vars.add(profit)
 		vars.addAll(trips)
-		logger.debug "vars=$vars"
-	}
-
-	private boolean resolveConstraints() {
+//		logger.debug "vars=$vars"
 		Search<IntVar> label = new DepthFirstSearch<IntVar>();
 		label.setPrintInfo(logger.isDebugEnabled());
 		SelectChoicePoint<IntVar> select = new InputOrderSelect<IntVar>(store, vars as IntVar[], new IndomainMax<IntVar>());
-		return label.labeling(store, select);
-	}
-	
-	private formatOptimisation(jsonTrips) {
-		def selectedTrips = []
-		trips.eachWithIndex { object, index ->
-			if(object.value() == 1) {
-				selectedTrips.add(jsonTrips[index].VOL)
+		if(label.labeling(store, select)) {
+			def selectedTrips = []
+			trips.eachWithIndex { object, index ->
+				if(object.value() == 1) {
+					selectedTrips.add(jsonTrips[index].VOL)
+				}
 			}
+			optimisation = [gain: profit.value(), path: selectedTrips]
 		}
-		optimisation = [gain: profit.value(), path: selectedTrips]
-	}
+//		logger.debug "optimisation=$optimisation"
+		return optimisation
+    }
 }
