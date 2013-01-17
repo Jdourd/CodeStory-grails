63f541427200b3be7507a19ae6667ec42398bbfa
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-18 00:20:22 +0100

    Trial with store.imposeDecomposition of a Sequence with state that only one (or none) var can be equal to one

--- grails-app/services/grails/story/TripSolverService.groovy
+++ grails-app/services/grails/story/TripSolverService.groovy
@@ -19,46 +19,58 @@ class TripSolverService {
 		def store = new Store()
 		def vars = []
 		def profit
-		def starts = []
-		def lengths = []
-		def choosedLengths = []
-		def ressources = []
+		def trips = []
 		def tripWeights = []
 		def optimisation = [gain: 0, path: []]
 	
 //		logger.debug "jsonTrips=$jsonTrips"
 		def tripCollisions = []
 		jsonTrips.sort{ it.DEPART }
-			.eachWithIndex { object, index ->
+				 .eachWithIndex { object, index ->
+			def trip = new BooleanVar(store, "t$index")
+			trips.add(trip)
 			tripWeights.add(object.PRIX)
-			
-			starts.add(new IntVar(store, "s$index", object.DEPART, object.DEPART))
-			def choosedLength = new IntVar(store, "cl$index", 0, 1)
-			choosedLengths.add(choosedLength)
-			def length = new IntVar(store, "l$index", object.DUREE, object.DUREE)
-			length.addDom(0, 0)
-			lengths.add(length)
-			store.impose(new XmulCeqZ(choosedLength, object.DUREE, length))
-			
-			ressources.add(new IntVar(store, "r$index", 1, 1))
-		}
-		IntVar limit = new IntVar(store, "limit", 0, 1);
-		Constraint ctr = new Cumulative(starts, lengths, ressources, limit)
-		store.impose(ctr)
-		
-		profit = new IntVar(store, "profit", 0, IntDomain.MaxInt)
-		store.impose(new SumWeight(choosedLengths, tripWeights, profit))
-		
-		vars.add(profit)
-		vars.addAll(choosedLengths)
 
+			for(int i = 0; i < object.DUREE; i++){
+				def hour = object.DEPART + i
+				if(tripCollisions[hour] == null) {
+					tripCollisions[hour] = []
+				}
+				tripCollisions[hour].add(trip)
+			}
+		}
+//		logger.debug "trips=$trips"
+//		logger.debug "tripWeights=$tripWeights"
+//		logger.debug "tripCollisions=$tripCollisions"
+		
+		def uniqueTripCollisions = new HashSet()
+		tripCollisions.each {
+			if(it  != null && it.size >= 2) {
+				uniqueTripCollisions.add(it)
+			}
+		}
+//		logger.debug "uniqueTripCollisions=$uniqueTripCollisions"
+
+		uniqueTripCollisions.each { uniqueTripCollision ->
+			store.imposeDecomposition(new Sequence(uniqueTripCollision as IntVar[], //variable list 
+                        new IntervalDomain(1,1), //set of values 
+                        uniqueTripCollision.size, // q, sequence length 
+                        0, // min 
+                        1  // max 
+                      ));
+		}
+		profit = new IntVar(store, "profit", 0, IntDomain.MaxInt);
+		store.impose(new SumWeight(trips, tripWeights, profit))
+		vars.add(profit)
+		vars.addAll(trips)
+//		logger.debug "vars=$vars"
 		Search<IntVar> label = new DepthFirstSearch<IntVar>();
 //		label.setPrintInfo(logger.isDebugEnabled());
 		label.setPrintInfo(false);
 		SelectChoicePoint<IntVar> select = new InputOrderSelect<IntVar>(store, vars as IntVar[], new IndomainMax<IntVar>());
 		if(label.labeling(store, select)) {
 			def selectedTrips = []
-			choosedLengths.eachWithIndex { object, index ->
+			trips.eachWithIndex { object, index ->
 				if(object.value() == 1) {
 					selectedTrips.add(jsonTrips[index].VOL)
 				}
@@ -68,5 +80,5 @@ class TripSolverService {
 		
 //		logger.debug "optimisation=$optimisation"
 		return optimisation
-    }
+	}
 }
