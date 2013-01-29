f9c81b1dfa30ec51e2cadbc0fae0c6be6aac4691
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-29 20:38:46 +0100

    Add time logger for JajascriptController

--- grails-app/controllers/grails/story/JajascriptController.groovy
+++ grails-app/controllers/grails/story/JajascriptController.groovy
@@ -2,9 +2,7 @@ package grails.story
 
 import org.apache.commons.logging.LogFactory
 import grails.converters.JSON
-import JaCoP.core.*
-import JaCoP.constraints.*
-import JaCoP.search.*
+import groovy.time.*
 
 class JajascriptController {
 
@@ -15,16 +13,23 @@ class JajascriptController {
 
     def optimize = {
 		def trips
+		def readJsonDate = new Date()
 		if(!request.JSON.isEmpty()) {
 			trips = request.JSON
-			logger.debug "json request=$trips"
+//			logger.debug "json request=$trips"
 		} else {
 			trips = params.find { true }.key // get first key
 			trips = JSON.parse trips
-			logger.debug "map request=$trips"
+//			logger.debug "map request=$trips"
 		}
+		def callSolverDate = new Date()
 		def optimisation = tripSolverService.solveTrips(trips)
-		logger.debug "optimisation=$optimisation"
+//		logger.debug "optimisation=$optimisation"
+		def stopDate = new Date()
+		TimeDuration jsonDuration = TimeCategory.minus(callSolverDate, readJsonDate)
+		TimeDuration solverDuration = TimeCategory.minus(stopDate, callSolverDate)
+		TimeDuration totalDuration = TimeCategory.minus(stopDate, readJsonDate)
+		logger.info "total=$totalDuration\tjson=$jsonDuration\tsolver=$solverDuration"
 		render optimisation as JSON
 	}
 }
