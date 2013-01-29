cd67db7f4a99124fb59f4034c294dabe014ad340
Gautier Dhordain <g.dhordain@gmail.com>
2013-01-29 22:56:04 +0100

    Read parameter log

--- grails-app/controllers/grails/story/JajascriptController.groovy
+++ grails-app/controllers/grails/story/JajascriptController.groovy
@@ -14,12 +14,13 @@ class JajascriptController {
 
     def optimize = {
 		def trips
-		def parsingJsonDate = new Date()
+		def readingParameterDate = new Date()
 		trips = request.getReader().text
 		if(trips.size() == 0){
 			trips = params.find { true }.key
 		}
-//		logger.info "map request : " + trips
+		
+		def parsingJsonDate = new Date()
 		trips = new ObjectMapper().readValue(trips, List.class)
 		logger.info "map request : " + trips.size()
 		
@@ -31,11 +32,12 @@ class JajascriptController {
 		def jsonOptimisation = new ObjectMapper().writeValueAsString(optimisation)
 		
 		def stopDate = new Date()
+		TimeDuration readingParameterDuration = TimeCategory.minus(parsingJsonDate, readingParameterDate)
 		TimeDuration parsingJsonDuration = TimeCategory.minus(callSolverDate, parsingJsonDate)
 		TimeDuration solverDuration = TimeCategory.minus(marshallingJsonDate, callSolverDate)
 		TimeDuration marshallingDuration = TimeCategory.minus(stopDate, marshallingJsonDate)
-		TimeDuration totalDuration = TimeCategory.minus(stopDate, parsingJsonDate)
-		logger.info "total=$totalDuration\tparsing=$parsingJsonDuration\tsolver=$solverDuration\tmarshalling=$marshallingDuration"
+		TimeDuration totalDuration = TimeCategory.minus(stopDate, readingParameterDate)
+		logger.info "total=$totalDuration\treading=$readingParameterDuration\tparsing=$parsingJsonDuration\tsolver=$solverDuration\tmarshalling=$marshallingDuration"
 		render jsonOptimisation
 	}
 }
